daf0fb7ba2250f4407217aad36891a855a05d391
frederic-baucher <frederic.baucher@insa-rouen.fr>
2021-05-28 19:38:20 +0200

    Create play.sql

---
+++ play.sql
@@ -0,0 +1,13 @@
+SELECT * FROM Employee ;
+
+INSERT INTO Employee (person_id, username, hiring_date, title, manager_id) VALUES (1, 'Rimlinger', '2005-10-06', 'manager', null) ;
+
+SELECT * FROM Employee ;
+
+SELECT * FROM Audit ;
+
+UPDATE Employee SET title = 'officer' ;
+
+SELECT * FROM Employee ;
+
+SELECT * FROM Audit ;
