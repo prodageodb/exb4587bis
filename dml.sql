265fd9eab2ebd8670ad00535a0074edaef29297f
frederic-baucher <frederic.baucher@insa-rouen.fr>
2021-05-28 21:06:35 +0200

    Create dml.sql

---
+++ dml.sql
@@ -0,0 +1,3 @@
+INSERT INTO Employee (person_id, username, hiring_date, title, manager_id) VALUES (1, 'Rimlinger', '2005-10-06', 'manager', null) ;
+INSERT INTO Employee (person_id, username, hiring_date, title, manager_id) VALUES (2, 'Bessin', '1977-04-08', 'analyst', 1) ;
+INSERT INTO Employee (person_id, username, hiring_date, title, manager_id) VALUES (3, 'Rudge', '2010-10-05', 'officer', 1) ;
